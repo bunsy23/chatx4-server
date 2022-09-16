2dd0c227bf97beea3fd21c7e926e6bce04cbc063
Hiromasy <bunsy2014@gmail.com>
2022-09-16 14:41:16 +0700

    add register user dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,31 @@
+import { CreateUserDto } from './dtos/createUser.dto';
 import { Routes, Services } from './../utils/types';
-import { Controller, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { IAuthService } from './auth';
 
 @Controller(Routes.AUTH)
 export class AuthController {
   constructor(@Inject(Services.AUTH) private authService: IAuthService) {}
+
+  @Post('register')
+  register(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+  }
+
+  @Post('login')
+  login() {}
+
+  @Get('status')
+  status() {}
+
+  @Post('logout')
+  logout() {}
 }
